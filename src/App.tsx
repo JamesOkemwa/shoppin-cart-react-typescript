a0799b8955df909b64c7cbece98428c5c1f68502
lejames <okejames55@gmail.com>
2022-09-26 16:10:20 +0300

    Shopping cartContext -> Shopping cart methods

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import { Home } from './pages/Home'
 import { About } from './pages/About'
 import { Store } from './pages/Store'
 import { Navbar } from './components/Navbar'
+import { ShoppingCartProvider } from "./context/ShoppingCartContext"
 
 function App() {
 
   return (
-    <>
-    <Navbar />
+    <ShoppingCartProvider>
+      <Navbar />
       <Container className="mb-4">
         <Routes>
           <Route path='/' element={<Home />} />
@@ -17,7 +18,7 @@ function App() {
           <Route path='/about' element={<About />} />
         </Routes>
       </Container>
-    </>
+    </ShoppingCartProvider>
     
   )
 }
